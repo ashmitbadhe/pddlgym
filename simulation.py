9bdbc4892888adf1f746ef36975c04567f6f0805
Ashmit Badhe <ashmitbadhe@gmail.com>
2024-12-01 17:33:22 +0530

    successfully parses domain operators

--- simulation.py
+++ simulation.py
@@ -3,7 +3,7 @@ from pddlgym.utils import run_demo
 
 def demo_random_perestroika():
     # Create the Sokoban environment
-    env = pddlgym.make("PDDLEnvPerestroika-v0")
+    env = pddlgym.make("PDDLEnvSokoban-v0")
 
     # Fix the problem index (optional for reproducibility)
     env.fix_problem_index(0)
@@ -15,7 +15,7 @@ def demo_random_perestroika():
     video_path = "perestroika_random_agent_10_steps.mp4"
 
     # Run the demo for exactly 5 steps with rendering
-    run_demo(env, policy, max_num_steps=10, render=True, video_path=video_path, fps=3, verbose=True)
+    run_demo(env, policy, max_num_steps=10, render=False, video_path=video_path, fps=3, verbose=False)
 
     print(f"Video saved to {video_path}")
 
