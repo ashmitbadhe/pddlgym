2ebc4d3893d00fa45c34d49df6764c0d8338098c
Ashmit Badhe <ashmitbadhe@gmail.com>
2025-02-10 01:04:02 +0100

    added new file nature.py

---
+++ pddlgym/nature.py
@@ -0,0 +1,8 @@
+import random
+import pddlgym.core as core
+class Nature:
+    def nature_KR2021(self, state, domain):
+        raise NotImplementedError("This function is not implemented yet")
+
+    def no_nature(state):
+        return state
